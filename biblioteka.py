560eda94a08e26a654e99a260a10442139545d5f
Marks06 <144447319+Marks06@users.noreply.github.com>
2023-09-29 11:39:39 +0300

    biblioteka.py

--- biblioteka.py
+++ biblioteka.py
@@ -1,17 +1,17 @@
-publikacija = (input('Vai grāmata ir publikācija? [Ja,Ne]:'))
+publikacija = (input('Vai šī ir publikācija? [Ja,Ne]:'))
 pieprasits = (input('Vai grāmata ir pieprasīto izdevumu sarakstā? [Ja,Ne]:'))
 gramatas = (input('Vai šī ir grāmata [ne žurnāls]? [Ja,Ne]:'))
 zurnals = (input('Vai šīs ir žurnāls? [Ja,Ne]:'))
 students = (input('Vai tu esi students? [Ja,Ne]:'))
 nenodots = (input('Vai tev ir nenodots izdevums? [Ja,Ne]:'))
 
-if publikacija == 'Ja' and pieprasits == 'Ja' and nenodots == 'Ne' and students == 'Ja' and students == 'Ne' and gramatas == 'Ja' or zurnals == 'Ja':
+if publikacija == 'Ja' and pieprasits == 'Ja' and nenodots == 'Ne' and students == 'Ja' and students == 'Ne' and gramatas == 'Ja' or zurnals == 'Ja': #ja atbilst visiem kritērijiem , tad printē ka izsniedz uz 2 dienām
     print('Izsniedz uz 2 dienām')
-elif gramatas == 'Ja' and pieprasits == 'Ne' and students == 'Ne' and nenodots == 'Ne':
+elif gramatas == 'Ja' and pieprasits == 'Ne' and students == 'Ne' and nenodots == 'Ne': #ja atbilst visiem kritērijiem , tad printē ka izsniedz uz 28 dienām
     print('izsniedzam uz 28 dienām')
-elif gramatas == 'Ja' and pieprasits == 'Ne' and students == 'Ja' and nenodots == 'Ne':
+elif gramatas == 'Ja' and pieprasits == 'Ne' and students == 'Ja' and nenodots == 'Ne': #ja atbilst visiem kritērijiem , tad printē ka izsniedz uz 14 dienām
     print('izsniedzam uz 14 dienām')
-elif zurnals == 'Ja' and pieprasits == 'Ne' and students == 'Ja' and students == 'Ne' and nenodots =='Ne':
+elif zurnals == 'Ja' and pieprasits == 'Ne' and students == 'Ja' and students == 'Ne' and nenodots =='Ne':  #ja atbilst visiem kritērijiem , tad printē ka izsniedz uz 7 dienām
     print('izsniedzam uz 7 dienām')
-elif nenodots == 'Ja':
+elif nenodots == 'Ja':    #ja atbilst visiem kritērijiem , tad printē ka neizsniedz vispār
     print('Nedrīksti neko ņemt')
